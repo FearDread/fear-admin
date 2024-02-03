0a304ce2763658ca03879f90b842ae0bd4d7d5a3
FearDread <ghaptonstall@gmail.com>
2024-02-03 19:51:25 +0000

    --fix express async handler and middleware calls

--- backend/src/middleware/index.js
+++ backend/src/middleware/index.js
@@ -1,7 +1,6 @@
 const config = require('../config');
 const nodeMailer = require('nodemailer');
 const AppError = require('../utils/app.error');
-const asyncWrapper = require('express-async-errors');
 
 exports.error = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
@@ -28,7 +27,7 @@ exports.error = (err, req, res, next) => {
     });
   };
 
-exports.send_email = asyncWrapper(async (options) => {
+exports.send_email = async (options) => {
     const transporter = nodeMailer.createTransport({
         host: config.SMTP_HOST,
         port: config.SMTP_PORT,
@@ -47,7 +46,7 @@ exports.send_email = asyncWrapper(async (options) => {
     };
 
     await transporter.sendMail(mailOptions);
-});
+};
 
 exports.send_jwt_token = (user, status, res) => {
     const token = user.getJWTToken();
