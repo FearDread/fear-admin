b8bc7bcb6232cb5e0ad604eef3e0a536e518f08a
FearDread <ghaptonstall@gmail.com>
2024-02-04 20:49:28 -0800

    --fix syntax

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -42,11 +42,9 @@ exports.createProduct = asyncWrapper(async (req, res) => {
     req.body.user = req.user.id;
     req.body.images = imagesLinks;
   }
-
   const data = await models.products.create(req.body);
 
-  res.status(200)
-     .json({ 
+  res.status(200).json({ 
         success: true,
         data: data 
       });
