a5f782d093a894691a81c24e746159bc5148b59d
FearDread <ghaptonstall@gmail.com>
2024-02-16 10:52:03 -0800

    --fix dotenv logic and added data folder for db

--- backend/src/auth/index.js
+++ backend/src/auth/index.js
@@ -1,29 +1,24 @@
 
 const models = require("../models");
+const User = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const AppError = require("../utils/app.error");
 const asyncWrapper = require('express-async-handler');
 
 exports.isAuthenticatedUser = asyncWrapper(async ( req, res, next ) => {
   const authHeader = req.headers.authorization;
-  const authCookie = req.cookies;
-
-  if ( !authHeader || !authCookie ) {
-    return next( new AppError("Unauthorized", 401) )
-  }
-
   const token = authHeader.split(' ')[1];
   
   try {
     const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
-    const user = await models.users.findById(deCodeToken.id);
+    const userDetails = await User.findById(deCodeToken.id);
 
-    req.user = user;
+    req.user = userDetails;
     next();
   
     } catch (err) {
       console.log(err).json();
-      app_error.error(401, "No Admin User Found");
+      AppError.error(401, "No Admin User Found");
     }
 });
 
--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,25 +1,30 @@
 const express  = require("express");
 const router  = express.Router();
+const Product = require("../controllers/product.controller");
 
-const { getAllProducts,
-    createProduct,
-        updateProduct,
-        deleteProduct,
-        getProductDetails,
-        getProductReviews,
-        deleteReview,
-        createProductReview,
-        getAllProductsAdmin } = require("../controllers/product.controller");
 const { isAuthentictedUser, authorizeRoles } = require("../auth");
  
-router.route("/product").get(getAllProducts)
-router.route("/admin/product/new").post(isAuthentictedUser, authorizeRoles("admin"), createProduct);
-router.route("/admin/products").get(isAuthentictedUser , authorizeRoles("admin") , getAllProductsAdmin);
+router.route("/product").get(Product.list)
+
+router.route("/admin/product/new")
+    .post(isAuthentictedUser, authorizeRoles("admin"), Product.create);
+
+router.route("/admin/products")
+    .get(isAuthentictedUser , authorizeRoles("admin") , Product.list);
+
 router.route("/admin/product/:id") 
-.put(isAuthentictedUser, authorizeRoles("admin"), updateProduct)
-.delete(isAuthentictedUser, authorizeRoles("admin"), deleteProduct);
-router.route("/product/:id").get(getProductDetails);
-router.route("/review/new").put(isAuthentictedUser , createProductReview);
-router.route("/reviews").get(getProductReviews) 
-router.route("/product/reviews/delete").delete(isAuthentictedUser , authorizeRoles("admin") ,deleteReview);
+    .put(isAuthentictedUser, authorizeRoles("admin"), Product.update)
+    .delete(isAuthentictedUser, authorizeRoles("admin"), deleteProduct);
+
+router.route("/product/:id").get(Product.read);
+
+router.route("/review/new")
+    .put(isAuthentictedUser , Product.createProductReview);
+
+router.route("/reviews")
+    .get(ProductgetProductReviews)
+     
+router.route("/product/reviews/delete")
+    .delete(isAuthentictedUser , authorizeRoles("admin") , Product.deleteReview);
+
 module.exports = router  
