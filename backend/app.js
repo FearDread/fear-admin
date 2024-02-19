526c8df373c0ba50dc914a3b2e72ef2828d9bfdf
FearDread <ghaptonstall@gmail.com>
2024-02-19 15:21:09 -0800

    --fix syntax error

--- backend/app.js
+++ backend/app.js
@@ -32,7 +32,7 @@ const __dirname1 = path.resolve();
 app.use(express.static(path.join(__dirname1, "/frontend")));
 
 app.get("*", (req, res) => {
-    console.log('API Route hit :: ' + res.url);
+    console.log('API Route hit :: ' + req.url);
     //res.redirect('http://fear.master.com:3000/home');
     res.sendFile(path.resolve(__dirname1, "frontend", "src", "index.js"))
 });
