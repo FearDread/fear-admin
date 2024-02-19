362b8afc5e89013301142555cbe5c8fd990e3211
FearDread <ghaptonstall@gmail.com>
2024-02-18 21:28:19 -0800

    Revert "--update package and build"
    This reverts commit 3b902dc62d4b9a8cf8c8828550b6f4592e462deb.

--- backend/app.js
+++ backend/app.js
@@ -32,8 +32,7 @@ const __dirname1 = path.resolve();
 app.use(express.static(path.join(__dirname1, "/frontend")));
 
 app.get("*", (req, res) =>
-  res.redirect('http://fear.master.com:3000/home')
-  //res.sendFile(path.resolve(__dirname1, "frontend", "src", "index.js"))
+  res.sendFile(path.resolve(__dirname1, "frontend", "src", "index.js"))
 );
 
 module.exports = app;
