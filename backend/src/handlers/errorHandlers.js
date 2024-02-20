5194800b16a55f2d9aabff2ddcc2406cefb990da
FearDread <ghaptonstall@gmail.com>
2024-02-20 15:28:28 -0800

    --update error handlers and db link in config

--- backend/src/handlers/errorHandlers.js
+++ backend/src/handlers/errorHandlers.js
@@ -5,10 +5,8 @@ class AppError extends Error {
 
     console.log(message)
   }
-
 }
 exports.AppError = AppError;
-
 /*
   Catch Errors Handler
 
@@ -33,7 +31,8 @@ exports.authError = (success, res, msg, jwt) => {
     message: msg,
     jwtExpired: jwt ? jwt : true
   });
-}
+};
+
 exports.errors = (res, msg, err) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     const message = '';
@@ -55,11 +54,6 @@ exports.errors = (res, msg, err) => {
     });
 };
 
-/*
-  Not Found Error Handler
-
-  If we hit a route that is not found, we mark it as 404 and pass it along to the next error handler to display
-*/
 exports.notFound = (req, res, next) => {
   res.status(404).json({
     success: false,
@@ -67,11 +61,6 @@ exports.notFound = (req, res, next) => {
   });
 };
 
-/*
-  Development Error Handler
-
-  In development we show good error messages so if we hit a syntax error or any other previously un-handled error, we can show good info on what happened
-*/
 exports.developmentErrors = (err, req, res, next) => {
   err.stack = err.stack || "";
   const errorDetails = {
@@ -89,11 +78,6 @@ exports.developmentErrors = (err, req, res, next) => {
   });
 };
 
-/*
-  Production Error Handler
-
-  No stacktraces are leaked to admin
-*/
 exports.productionErrors = (err, req, res, next) => {
   res.status(500).json({
     success: false,
