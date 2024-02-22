30370e0be28b13be9a48f4367cb6b9ac1ce04788
FearDread <ghaptonstall@gmail.com>
2024-02-22 15:10:38 -0800

    --add animated background component

---
+++ dashboard/src/views/components/AnimatedBackground.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const AnimatedBackground = () => {
+
+    return (
+        <>
+            <div className="animated-background">    
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>   
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>   
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </>
+    );
+
+}
+
+export default AnimatedBackground;
