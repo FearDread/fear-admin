1ea029746e56c16aa58b6609d0b8ce16d3ecb1f4
FearDread <ghaptonstall@gmail.com>
2024-02-23 17:29:07 -0800

    --add loading styles and update dashboard, product, user list views

--- dashboard/src/components/Loader/Loading.js
+++ dashboard/src/components/Loader/Loading.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ReactComponent as CogWheel } from "../../assets/img/spinner.svg";
+import "../../assets/css/loading.css";
 
 const CogWheelLoader = () => (
   <div className="cog-loader">
