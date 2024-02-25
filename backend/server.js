a284ec28e052f9d0578dfdae1bd385134ad74143
FearDread <ghaptonstall@gmail.com>
2024-02-24 19:39:16 -0800

    --fix session logic in main app

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 #!/usr/bin/env node
-
 const app = require("./app");
 const db = require("./src/data/db");
 const cloudinary = require("cloudinary");
+const session = require("express-session");
 require("dotenv").config({ path: "./.env" });
 
 // connect to db //
 db.run();
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: db.store()
+  })
+)
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
