07918dda913b91904158434c196b597bae7e2400
FearDread <ghaptonstall@gmail.com>
2024-02-24 21:56:49 -0800

    --fix user route logic

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,24 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../controllers/user");
-const { isAuthenticated,
-        authorizeRoles,
-        isValidToken, } = require("../auth");
-const { catchErrors, authError } = require("../_helpers/errorHandlers");
+const { isAuthenticated, authorizeRoles } = require("../auth");
+const { catchErrors } = require("../_helpers/errorHandlers");
 
 
 router.route("/register").post(catchErrors(User.register));
 router.route("/profile").get(isAuthenticated, catchErrors(User.read));
 router.route("/password/forgot").post(User.forgotPassword);
 router.route("/password/update").put(isAuthenticated, User.updatePassword);
-router.route("/profile/update").put(isValidToken, User.update);
+router.route("/profile/update").put(isAuthenticated, User.update);
 router.route("/password/reset/:token").put(catchErrors(User.resetPassword));
 
 router.route("/admin/users")
-    .get(isValidToken, authorizeRoles("admin"), catchErrors(User.list));
+    .get(isAuthenticated, authorizeRoles("admin"), catchErrors(User.list));
 router.route("/admin/user/:id")
-    .get(isValidToken, authorizeRoles("admin"), catchErrors(User.read))
-    .delete(isValidToken , authorizeRoles("admin") , catchErrors(User.delete))
-    .put(isValidToken , authorizeRoles("admin") , catchErrors(User.updateRole));
+    .get(isAuthenticated, authorizeRoles("admin"), catchErrors(User.read))
+    .delete(isAuthenticated , authorizeRoles("admin") , catchErrors(User.delete))
+    .put(isAuthenticated , authorizeRoles("admin") , catchErrors(User.updateRole));
 
 module.exports = router;
