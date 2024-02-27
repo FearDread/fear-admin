76fad81fdfa361ecf5caa9e95ab7310323534f02
FearDread <ghaptonstall@gmail.com>
2024-02-27 09:49:49 -0800

    --continue working on login / is admin state logic

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -6,14 +6,14 @@ const { catchErrors } = require("../_helpers/errorHandlers");
 const asyncHandler = require("../_helpers/asyncHandler");
 
 router.route("/register").post(catchErrors(User.register));
-router.route("/profile").get(isAuthenticated, asyncHandler(User.readUser));
+router.route("/profile").get(isAuthenticated, catchErrors(User.readUser));
 router.route("/password/forgot").post(User.forgotPassword);
 router.route("/password/update").put(isAuthenticated, User.updatePassword);
 router.route("/profile/update").put(isAuthenticated, User.update);
 router.route("/password/reset/:token").put(catchErrors(User.resetPassword));
 
 router.route("/admin/users")
-    .get(isAuthenticated, asyncHandler(User.list));
+    .get(catchErrors(User.list));
 router.route("/admin/user/:id")
     .get(isAuthenticated, authorizeRoles("admin"), catchErrors(User.read))
     .delete(isAuthenticated , authorizeRoles("admin") , catchErrors(User.delete))
