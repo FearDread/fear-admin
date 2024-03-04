263178051fa2617ed11fa6a9b857fc3a5c581377
FearDread <ghaptonstall@gmail.com>
2024-03-04 00:36:20 -0800

    --restart with base dashboard tmeplate files

--- dashboard/src/components/FixedPlugin/FixedPlugin.js
+++ dashboard/src/components/FixedPlugin/FixedPlugin.js
@@ -1,3 +1,19 @@
+/*!
+
+=========================================================
+* Black Dashboard PRO React - v1.2.1
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/black-dashboard-pro-react
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
 import React from "react";
 
 import { Button, CustomInput } from "reactstrap";
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,26 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-import App from "App.js";
+import AuthLayout from "layouts/Auth/Auth.js";
+import AdminLayout from "layouts/Admin/Admin.js";
+import RTLLayout from "layouts/RTL/RTL.js";
+
+import {Provider } from "react-redux";
+import store from "_redux/store";
+
 import "assets/css/nucleo-icons.css";
 import "react-notification-alert/dist/animate.css";
 import "assets/scss/black-dashboard-pro-react.scss?v=1.2.0";
 import "assets/demo/demo.css";
 import "assets/css/animated-bg.css";
+import "assets/css/loading.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <BrowserRouter>
-      <Provider store={store}>
-        
-        <App />
+  <BrowserRouter>
+    <Provider store={store}>
+    <Switch>
+      <Route path="/auth" render={(props) => <AuthLayout {...props} />} />
+      <Route path="/admin" render={(props) => <AdminLayout {...props} />} />
+      <Redirect from="/" to="/admin/dashboard" />
+    </Switch>
+    </Provider>
+  </BrowserRouter>
 
-      </Provider>
-    </BrowserRouter>
-)
-
-
+);
