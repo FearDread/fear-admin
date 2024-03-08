d562841f56237343dd44c14dc95c3bfde02a9ccd
FearDread <ghaptonstall@gmail.com>
2024-03-08 05:46:12 -0800

    --fix login /register .. updated sidebar routes

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-import AuthLayout from "layouts/Auth/Auth.js";
-import AdminLayout from "layouts/Admin/Admin.js";
-
 import App from "./App";
 import {Provider } from "react-redux";
 import store from "_redux/store";
