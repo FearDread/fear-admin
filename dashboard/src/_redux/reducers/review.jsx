b9eace51beec73713b086975ea3d469ea36fcb23
FearDread <ghaptonstall@gmail.com>
2024-03-17 20:18:28 -0700

    --add review reducer

---
+++ dashboard/src/_redux/reducers/review.jsx
@@ -0,0 +1,113 @@
+import * as types from "../types/product";
+
+const initialState = {
+    state: {},
+    reviews: [],
+    review: {},
+}
+
+
+export const reviewReducer = (state = {}, action) => {
+    switch (action.type) {
+      case NEW_REVIEW_REQUEST:
+        return {
+          ...state,
+          loading: true,
+        };
+      case NEW_REVIEW_SUCCESS:
+        return {
+          loading: false,
+          success: action.payload,
+        };
+  
+      case NEW_REVIEW_FAIL:
+        return {
+          loading: false,
+          error: action.payload,
+        };
+  
+      case NEW_REVIEW_RESET:
+        return {
+          ...state,
+          success: false,
+        };
+  
+      case CLEAR_ERRORS:
+        return {
+          ...state,
+          error: null,
+        };
+  
+      default:
+        return state;
+    }
+  };
+
+  // get all review Reducer =>
+
+export const getALLReviewReducer = (state = { reviews: [] }, action) => {
+    switch (action.type) {
+      case ALL_REVIEW_REQUEST:
+        return {
+          ...state,
+          loading: true,
+        };
+      case ALL_REVIEW_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          reviews: action.payload,
+        };
+      case ALL_REVIEW_FAIL:
+        return {
+          ...state,
+          loading: false,
+          error: action.payload,
+        };
+      case CLEAR_ERRORS:
+        return {
+          ...state,
+          error: null,
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  // delete review reducer  =>
+  
+  export const deleteReviewReducer = (state = {}, action) => {
+    switch (action.type) {
+      case DELETE_REVIEW_REQUEST:
+        return {
+          ...state,
+          loading: true,
+        };
+      case DELETE_REVIEW_SUCCESS:
+        return {
+          ...state,
+          loading: false,
+          isDeleted: action.payload,
+        };
+  
+      case DELETE_REVIEW_FAIL:
+        return {
+          ...state,
+          loading: false,
+          error: action.payload,
+        };
+      case DELETE_REVIEW_RESET:
+        return {
+          ...state,
+          isDeleted: false,
+        };
+      case CLEAR_ERRORS:
+        return {
+          ...state,
+          error: null,
+        };
+      default:
+        return state;
+    }
+  };
