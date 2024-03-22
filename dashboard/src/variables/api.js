f8ce5e74eebb89436290838fed0b850337472384
FearDread <ghaptonstall@gmail.com>
2024-03-22 11:54:56 +0000

    --fix package and env files

--- dashboard/src/variables/api.js
+++ dashboard/src/variables/api.js
@@ -1,6 +1,9 @@
+export const API_BASE_URL = "http://fear.master.com:4000/fear/api";
+/*
 export const API_BASE_URL = process.env.NODE_ENV === "production"
                 ? "http://fear.master.com:4000/fear/api"
                 : "http://localhost:4000/fear/api";
+		*/
   
 export const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
                 ? process.env.JWT_TOKEN 
