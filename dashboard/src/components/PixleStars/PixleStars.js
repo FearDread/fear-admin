0eabc21cb6674ed513635dc24bfe0db87a400de4
FearDread <ghaptonstall@gmail.com>
2024-03-25 00:21:53 -0700

    --add new pixle star animated background component

---
+++ dashboard/src/components/PixleStars/PixleStars.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./PixleStars.scss";
+
+const PixleStars = () => {
+
+    return (
+        <>
+        <div className="pixle-bg">
+            <div className="container">
+                <img src="assets/img/dark-night.png" />
+                <div className="circle-container">
+                    <div className="circle"></div>
+                </div>
+                <div className="circle-container">
+                    <div className="circle"></div>
+                </div>
+                <div className="circle-container">
+                    <div className="circle"></div>
+                </div>
+                <div className="circle-container">
+                    <div className="circle"></div>
+                </div>
+                <div className="circle-container">
+                    <div className="circle"></div>
+                </div>
+                <div className="circle-container">
+                    <div className="circle"></div>
+                </div>
+                <div className="circle-container">
+                    <div className="circle"></div>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default PixleStars;
