35a74aa560326adcc2861099c1c9cb18b35997f0
FearDread <ghaptonstall@gmail.com>
2024-05-03 08:51:31 -0700

    --fix user controller and route, update ekomix product calls

--- ekomix/src/_config/api.js
+++ ekomix/src/_config/api.js
@@ -1,7 +1,10 @@
 
 
 console.log('process.env = ', process.env);
+/*
 export const API_BASE_URL = process.env.NODE_ENV === "production" 
     ? "http://fear.master.com:4000/fear/api" 
     : "http://localhost:4000/fear/api";
+*/
+export const API_BASE_URL = "http://fear.master.com:4000/fear/api";
 
