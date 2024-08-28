4a4c5ef4d936147304c9d295eafe5b02e4b5dd20
FearDread <ghaptonstall@gmail.com>
2024-08-28 01:09:03 -0700

    --remove server dir and update package.json

--- backend/src/boot.js
+++ backend/src/boot.js
@@ -1,13 +1,6 @@
-import fs from "fs";
+const cloudinary = require("cloudinary");
 
 module.exports = app => {
-
-    if (process.env.NODE_ENV !== "test") {
-        const credentials = {
-            key: fs.readFileSync("ntask.key", "utf8"),
-            cert: fs.readFileSync("ntask.cert", "utf8")
-        }
-    }  
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.API_KEY,
