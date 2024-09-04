669211b2c2c3ff4492d1ae734878e983809c4353
FearDread <ghaptonstall@gmail.com>
2024-09-03 21:39:39 -0700

    --finish new FEAR server logic implimenting dynamic module loading

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,15 @@
-
-const app = require("./src/app");
-const db = require("./src/libs/db");
 const cloudinary = require("cloudinary");
-require("dotenv").config({ path: ".env" });
-const PORT = app.get("PORT") || 5000;
-
-db.run();
+const FEAR = require("./src/app");
+require("dotenv").config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET,
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
 });
 
-app.listen(PORT, () => {
-  console.log(`FEAR API Server is listening on PORT ${PORT}`);
+FEAR.db.run();
+FEAR.app.listen( FEAR.app.get("PORT"), () => {
+    console.log(`FEAR API Initialized :: Port ${FEAR.app.get("PORT")}`);
 });
 
