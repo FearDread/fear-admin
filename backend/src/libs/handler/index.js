6053ac99a9c609d0b296733136470dad73e820d0
FearDread <ghaptonstall@gmail.com>
2024-09-05 06:55:20 -0700

    --finish startup logic for main FEAR obj and fix libs

--- backend/src/libs/handler/index.js
+++ backend/src/libs/handler/index.js
@@ -1,7 +1,3 @@
-
-
-
-
 /*
   Catch Errors Handler
 
@@ -9,7 +5,7 @@
   Instead of using try{} catch(e) {} in each controller, we wrap the function in
   catchErrors(), catch any errors they throw, and pass it along to our express middleware with next()
 */
-exports.wrapper = (fn) => {
+exports.catchError = (fn) => {
     return function (req, res, next) {
       const resp = fn(req, res, next);
       if (resp instanceof Promise) {
@@ -23,7 +19,7 @@ exports.wrapper = (fn) => {
     Not Found Error Handler
     If we hit a route that is not found, we mark it as 404 and pass it along to the next error handler to display
 */
-exports.unknown = ( req, res ) => {
+exports.notFound = ( req, res ) => {
     return res.status(StatusCodes.NOT_FOUND).send("Route does not exist");
 };
 
@@ -32,7 +28,7 @@ exports.unknown = ( req, res ) => {
   
     In development we show good error messages so if we hit a syntax error or any other previously un-handled error, we can show good info on what happened
   */
-exports.development = (err, req, res, next) => {
+exports.devError = (err, req, res, next) => {
     err.stack = err.stack || "";
     const details = {
       message: err.message || "Server Error",
@@ -49,6 +45,6 @@ exports.development = (err, req, res, next) => {
     Production Error Handler 
     No stacktraces are leaked to admin
   */
-exports.production = (err, req, res, next) => {
+exports.prodError = (err, req, res, next) => {
     res.status(500).json({ success: false, message: "Error in Server" });
 };
