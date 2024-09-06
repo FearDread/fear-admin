a504ee9df080759b45e6cef5d7ef81940a93d28c
FearDread <ghaptonstall@gmail.com>
2024-09-06 08:37:39 -0700

    --fix base api url in variables

--- dashboard/src/variables/api.js
+++ dashboard/src/variables/api.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL = process.env.NODE_ENV === "production"
-                ? "http://fear.master.com:4000/fear/api"
-                : "http://fear.master.com:4000/fear/api"; 
+                ? "localhost:4000/fear/api"
+                : "172.128.10.101:4000/fear/api"; 
   
 export const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
                 ? process.env.JWT_TOKEN 
