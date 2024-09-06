fe2e10d4fa4613ba668fce6530ca1e9ad30ddfbd
FearDread <ghaptonstall@gmail.com>
2024-09-06 08:57:13 -0700

    --add correct http protocol to api url

--- dashboard/src/variables/api.js
+++ dashboard/src/variables/api.js
@@ -1,6 +1,6 @@
 export const API_BASE_URL = process.env.NODE_ENV === "production"
-                ? "localhost:4000/fear/api"
-                : "172.128.10.101:4000/fear/api"; 
+                ? "http://172.128.10.101:4000/fear/api" 
+                : "http://localhost:4000/fear/api"; 
   
 export const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
                 ? process.env.JWT_TOKEN 
