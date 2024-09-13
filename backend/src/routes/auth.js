4c72c620a4594acb737fc81827893e83c03d9edd
FearDread <ghaptonstall@gmail.com>
2024-09-13 06:27:39 -0700

    --fix auth routes

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,11 +3,7 @@ const router = express.Router();
 const { asyncHandler } = require("../libs/handler");
 const { login, logout } = require("../controllers/auth");
 
-//router.route("/login").post( asyncHandler(login) );
-//router.route("/logout").post( asyncHandler(logout) );
+router.route("/login").post( asyncHandler(login) );
+router.route("/logout").post( asyncHandler(logout) );
 
-module.exports = ( app ) => {
-    app.post("/login", asyncHandler(login));
-
-    app.post("/logout", asyncHandler(logout));
-}
+module.exports = router;
