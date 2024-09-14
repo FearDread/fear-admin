3e217e62581354b9d5a4e08eca06b7df8220388e
FearDread <ghaptonstall@gmail.com>
2024-09-14 05:00:56 -0700

    --finish resolving dependency calls in routes and controllers

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const User = require("../controllers/Order");
-const { isAuthorized, isAdmin } = require("../lbis/auth");
+const User = require("../controllers/user");
+const { isAuthorized, isAdmin } = require("../libs/auth");
 
+/*
 router.get("/wishlist", isAuthorized, User.getWishlist);
 router.get("/cart", isAuthorized, User.getUserCart);
 router.get("/:id", isAuthorized, isAdmin, User.getaUser);
+*/
 
 module.exports = router;
