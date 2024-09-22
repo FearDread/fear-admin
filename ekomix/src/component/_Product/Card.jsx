3cae6669005df3c681483e8b80089b23e0962727
FearDread <ghaptonstall@gmail.com>
2024-09-21 17:31:42 -0700

    --temp remove stripe calls and fix main product list calls on home and shop pages

--- ekomix/src/component/_Product/Card.jsx
+++ ekomix/src/component/_Product/Card.jsx
@@ -14,13 +14,14 @@ import {
 } from "reactstrap";
 import Rating from "@material-ui/lab/Rating";
 import { addItemToCart } from "../../_store/actions/cartAction";
+import logo from "assets/img/ekomix/logo.png";
 
 
 const ProductCard = ({ product }) => {
   const dispatch = useDispatch();
 
-  const truncated = product.description.split(" ").slice(0, 5).join(" ") + "...";
-  const nameTruncated = product.name.split(" ").slice(0, 3).join(" ") + "...";
+  //const truncated = product.description.split(" ").slice(0, 5).join(" ") + "...";
+  //const nameTruncated = product.name.split(" ").slice(0, 3).join(" ") + "...";
   
   const addTocartHandler = (id , qty) => {
     dispatch(addItemToCart(id , qty))
@@ -31,12 +32,12 @@ const ProductCard = ({ product }) => {
       <Link to={`/product/${product._id}`}>
         <Card className="card-product card-home">
           <div className="card-image">
-            <img alt="..." class="img rounded" src={product.images[0] && product.images[0].url ? product.images[0].url : ''} />
+            <img alt="..." class="img rounded" src={product.images[0] ? product.images[0].url : logo} />
           </div>
           <div class="card-body">
             <h6 class="category text-warning">Popular</h6>
-            <h4 class="card-title">{nameTruncated}</h4>
-            <div class="card-description"><span>{truncated}</span></div>
+            <h4 class="card-title">{product.name}</h4>
+            <div class="card-description"><span>{product.description}</span></div>
             <div class="card-footer">
               <div class="price-container">
                 <span class="price">${product.price}</span>
