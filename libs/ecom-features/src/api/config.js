e0839bea20cc172fb1d9e606893bdd4a6e3407bf
FearDread <ghaptonstall@gmail.com>
2024-09-27 08:23:38 -0700

    --add new ecom-features lib to consolodate redux api actions

---
+++ libs/ecom-features/src/api/config.js
@@ -0,0 +1,21 @@
+import storePersist from "../libs/persist";
+
+const isAuth = storePersist.get("auth") ? storePersist.get("auth") : null;
+
+export const AXIOS_CONFIG = {
+  headers: {
+    Authorization: `Bearer ${
+      isAuth !== null ? isAuth.token : ""
+    }`,
+    Accept: "application/json",
+    "Content-Type": "multipart/form-data"
+  },
+};
+
+export const API_BASE_URL = process.env.NODE_ENV === "production"
+                ? "http://172.128.10.101:4000/fear/api" 
+                : "http://localhost:4000/fear/api"; 
+  
+export const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
+                ? process.env.JWT_TOKEN 
+                : "x-auth-token";
---
+++ libs/ecom-features/src/index.js
@@ -0,0 +1,5 @@
+
+
+export { default as API } from "./api/actions";
+export { default as rootReducer } from "./libs/rootReducer";
+
