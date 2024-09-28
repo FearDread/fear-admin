8fd964085386061085ff2e3fd12ea58a879cc187
FearDread <ghaptonstall@gmail.com>
2024-09-28 13:55:46 -0700

    --update crud-service package with selectors and auth, update package version

---
+++ libs/crud-service/src/store/store.jsx
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { thunk } from "redux-thunk";
+import { createLogger } from "redux-logger";
+import rootReducer from "./reducer.jsx";
+import storePersist from "./persist.jsx";
+
+const logger = createLogger();
+
+let middleware = [thunk];
+let configStore = applyMiddleware(...middleware);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+if (process.env.NODE_ENV === "development") {
+  middleware = [...middleware, logger];
+  configStore = composeEnhancers(applyMiddleware(...middleware));
+}
+
+const initialState = storePersist.get("auth")
+  ? { auth:storePersist.get("auth") }
+  : {};
+
+const store = createStore(rootReducer, initialState, configStore);
+
+export default store;
