3b624b9b4ccf4d3d8f7a6152d7b759b399bd49d9
FearDread <ghaptonstall@gmail.com>
2024-10-08 16:10:13 -0700

    --add needed img assets and update api calls using crud service

--- e-komix/src/components/Home/Instagram/Instagram.jsx
+++ e-komix/src/components/Home/Instagram/Instagram.jsx
@@ -1,63 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./Instagram.css";
-import insta1 from "../../../Assets/Instagram/insta1.jpg";
-import insta2 from "../../../Assets/Instagram/insta2.jpg";
-import insta3 from "../../../Assets/Instagram/insta3.jpg";
-import insta4 from "../../../Assets/Instagram/insta4.jpg";
-import insta5 from "../../../Assets/Instagram/insta5.jpg";
-import insta6 from "../../../Assets/Instagram/insta6.jpg";
-import insta7 from "../../../Assets/Instagram/insta7.jpg";
-import insta8 from "../../../Assets/Instagram/insta8.jpg";
-import insta9 from "../../../Assets/Instagram/insta9.jpg";
-import insta10 from "../../../Assets/Instagram/insta10.jpg";
-import insta11 from "../../../Assets/Instagram/insta11.jpg";
-import insta12 from "../../../Assets/Instagram/insta12.jpg";
+import { CRUD } from "@feardread/crud-service";
 
 const Instagram = () => {
+  const dispatch = useDispatch();
+  const { loading, result } = useSelector((state) => state.crud.list);
+  
+  const getProductImages = () => {
+    dispatch(CRUD.all('product'));
+  }
+
+  useEffect(() => {
+    //dispatch(CRUD.all('product'));
+    console.log("products = ", result);
+
+  }, [dispatch]);
+  
+  
   return (
     <>
+    {loading ? ( 
+      <>
       <div className="instagram">
-        <h2>@UOMO</h2>
+      <h2>@ E-Komix</h2>
+      </div>
+      </>
+    ) : (
+    <>
+      <div className="instagram">
+        <h2>@ E-Komix</h2>
         <div className="instagramTiles">
-          <div className="instagramtile">
-            <img src={insta1} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta2} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta3} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta4} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta5} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta6} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta7} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta8} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta9} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta10} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta11} alt="" />
-          </div>
-          <div className="instagramtile">
-            <img src={insta12} alt="" />
-          </div>
+        {result && result.map((product) => {
+            <div className="instagramtile">
+              <img 
+              src={product.images[0] ? product.images[0].url : ""} 
+              alt=""
+               />
+            </div>
+          })
+        }
         </div>
       </div>
     </>
+    )}
+  </>
   );
 };
 
--- e-komix/src/pages/Home.jsx
+++ e-komix/src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Banner from "../Components/Home/Banner/Banner";
 import CollectionBox from "../Components/Home/Collection/CollectionBox";
 import Services from "../Components/Home/Services/Services";
@@ -9,10 +10,19 @@ import DealTimer from "../Components/Home/Deal/DealTimer";
 import HeroSection from "../Components/Home/Hero/HeroSection";
 import AnimatedHero from "../Components/AnimatedHero/AnimatedHero";
 
+import { CRUD } from "@feardread/crud-service";
+
 const Home = () => {
+  const dispatch = useDispatch();
+  const { loading, result } = useSelector((state) => state.crud.list);
+
+  useEffect(() => {
+    dispatch(CRUD.all('product'));
+  }, [dispatch])
+
+
   return (
     <>
-{/*      <HeroSection /> */}
       <AnimatedHero />
       <CollectionBox />
       <Trendy />
