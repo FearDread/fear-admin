c35617cfd78183beab43b9dc9a05fe57a88b5dfc
FearDread <ghaptonstall@gmail.com>
2024-10-16 19:37:51 -0700

    --update e-komix asset locations and api calls

--- e-komix/src/App.js
+++ e-komix/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import "./App.css";
+import "./Assets/App/App.css";
 
 import Home from "../src/Pages/Home";
 import About from "../src/Pages/About";
--- e-komix/src/index.js
+++ e-komix/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { STORE } from "@feardread/crud-service";
-import "./index.css";
+import "./Assets/App/index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
