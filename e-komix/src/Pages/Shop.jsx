94072eb6179d5aa0e03bd82dee875ec9ea77d64b
FearDread <ghaptonstall@gmail.com>
2024-10-19 15:24:02 +0000

    fix dir names

---
+++ e-komix/src/Pages/Shop.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import ShopDetails from "../Components/Shop/ShopDetails/ShopDetails";
+import { cruds } from "@feardread/crud-service";
+import Loader from "../Components/Loader/Loader";
+
+const Shop = () => {
+  const dispatch = useDispatch();
+  const { loading, result } = useSelector((state) => state.crud.list);
+
+  const getTrendyProducts = () => {
+    dispatch(cruds.all('product'));
+  }
+
+  useEffect(() => {
+
+    getTrendyProducts();
+
+  }, []);
+
+  return (
+    <>
+    {loading ? (
+      <>
+        <Loader />
+      </>
+    ) : (
+      <>
+        <ShopDetails {...result}/>
+      </>
+    )}
+  </>
+  )
+};
+
+export default Shop;
