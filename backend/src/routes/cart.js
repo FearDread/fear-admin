8e2ab1f0faf640acaea6ecea8a990bfe064cb815
FearDread <ghaptonstall@gmail.com>
2024-10-24 18:24:01 -0700

    --add cart functions to cart controller and minor crud updates

--- backend/src/routes/cart.js
+++ backend/src/routes/cart.js
@@ -2,8 +2,8 @@ const router = require("express").Router();
 const Cart = require("../controllers/cart");
 const { isAuthorized, isAdmin } = require("../controllers/auth");
 
-router.post("/new", Cart.create);
-router.get("/mycart", Cart.getUserCart);
+router.post("/new", Cart.add);
+router.get("/mycart", Cart.getCart);
 
 router.route("/:id")
     .get(Cart.read)
