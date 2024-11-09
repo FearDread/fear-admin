8b80d61e1609b7735a9591cd46427d8497bb8ff2
FearDread <ghaptonstall@gmail.com>
2024-11-08 20:02:57 -0800

    --update e-komix product and auth pages

--- e-komix/src/pages/Authentication.jsx
+++ e-komix/src/pages/Authentication.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import LoginSignUp from "../Components/Authentication/LoginSign/LoginSignUp";
+import Profile from "../Components/User/Profile";
 
 const Authentication = () => {
   return (
