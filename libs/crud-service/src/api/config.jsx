baae4724ffa0b79f9f8005e4bcbbef06b976bd90
FearDread <ghaptonstall@gmail.com>
2024-11-16 21:08:52 -0800

    --update cruds lib logic to account for production build state;

--- libs/crud-service/src/api/config.jsx
+++ libs/crud-service/src/api/config.jsx
@@ -1,6 +1,12 @@
 import StorePersist from "../store/StorePersist.jsx";
 
+const source = process.env.NODE_ENV === "production" ? "http://13.59.85.143:4000" : "http://localhost:4000";
 const isAuth = StorePersist.get("auth") ? StorePersist.get("auth") : null;
+
+export const API_BASE_URL = source  + "/fear/api";
+export const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
+                ? process.env.JWT_TOKEN 
+                : "x-auth-token";
 
 export const AXIOS_CONFIG = {
   headers: {
@@ -11,11 +17,3 @@ export const AXIOS_CONFIG = {
     "Content-Type": "multipart/form-data"
   },
 };
-
-export const API_BASE_URL = process.env.NODE_ENV === "production"
-                ? "http://fear.master.com:4000/fear/api" 
-                : "http://localhost:4000/fear/api"; 
-  
-export const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
-                ? process.env.JWT_TOKEN 
-                : "x-auth-token";
