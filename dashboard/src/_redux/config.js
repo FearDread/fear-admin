7c67a2b0db1aa9b6aeba2cbe087425552f2d29f7
FearDread <ghaptonstall@gmail.com>
2024-12-27 03:50:48 -0800

    --update redux config to use https for production api

--- dashboard/src/_redux/config.js
+++ dashboard/src/_redux/config.js
@@ -14,8 +14,8 @@ export const AXIOS_CONFIG = {
 
 export const API_BASE_URL = process.env.NODE_ENV === "production"
                 ? "https://fear.master.com:4000/fear/api" 
-                : "http://localhost:4000/fear/api"; 
-  
+                : "http://localhost:4000/fear/api";
+ 
 export const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
                 ? process.env.JWT_TOKEN 
                 : "x-auth-token";
