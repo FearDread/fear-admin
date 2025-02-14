1ed246cac8a907a3714296e4f7beee2ea579384d
FearDread <ghaptonstall@gmail.com>
2025-02-14 09:25:24 -0800

    --update redux actions to use new API instance

--- dashboard/src/_redux/config.js
+++ dashboard/src/_redux/config.js
@@ -13,7 +13,7 @@ export const AXIOS_CONFIG = {
 };
 
 export const API_BASE_URL = process.env.NODE_ENV === "production"
-                ? "https://fear.master.com:4000/fear/api" 
+                ? "http://fear.master.com:4000/fear/api" 
                 : "http://localhost:4000/fear/api";
  
 export const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
