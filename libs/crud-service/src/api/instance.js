3d1889e9dc99aed387ed5184202659e5d5a7988e
FearDread <ghaptonstall@gmail.com>
2025-02-28 11:30:09 -0800

    --update crud-service lib with updated API and rebuild / publish

--- libs/crud-service/src/api/instance.js
+++ libs/crud-service/src/api/instance.js
@@ -16,9 +16,14 @@ const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN)
 
 const instance = axios.create({
     baseURL: `${API_BASE_URL}`,
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
     paramsSerializer: (params) => {
         return qs.stringify(params, { indices: false });
     },
+    credentials: true
     //httpsAgent: new https.Agent({ rejectUnauthorized: false })
 });
 
@@ -28,8 +33,6 @@ instance.interceptors.request.use(
         let token = isAuth !== null ? isAuth.token : "";
     
         config.headers = {
-            Accept: "application/json",
-            "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
             [ACCESS_TOKEN_NAME]: token
         };
@@ -67,18 +70,6 @@ instance.interceptors.response.use(
 );
 
 
-export const API = setupCache(instance, {
-    ttl: 1000 * 60 * 15,
-    interpretHeader: false,
-    methods: ['get'],
-    cachePredicate: {
-      statusCheck: (status) => status >= 200 && status < 400,
-    },
-    update: {},
-    etag: false,
-    modifiedSince: false,
-    staleIfError: false,
-    debug: console.log
-})
+export const API = instance;
 
 export default API;
