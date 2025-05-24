02d4b7208a8d98458718699f4539464888679372
FearDread <ghaptonstall@gmail.com>
2025-05-24 11:32:31 -0700

    --fix auth login route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -3,8 +3,8 @@ const router = require('express').Router();
 const User = require("../controllers/user");
 const { login, logout, register, passportLogin } = require("../controllers/auth");
 
-//router.post("/login", tryCatch(login));
-router.post('/login', tryCatch(passportLogin));
+router.post("/login", tryCatch(login));
+//router.post('/login', tryCatch(passportLogin));
 router.post("/logout", tryCatch(logout));
 router.post("/register", tryCatch(register));
 
