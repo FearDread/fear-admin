de6d83cc3dd9c1662e575da9e506fc412bff6982
FearDread <ghaptonstall@gmail.com>
2025-05-23 20:51:10 -0700

    --remove crud-service from package and update dashboard api instance

--- dashboard/src/_redux/api/instance.js
+++ dashboard/src/_redux/api/instance.js
@@ -5,7 +5,7 @@ import storePersist from "../storePersist";
 
 const API_URL = (process.env.NODE_ENV === "production")
                  ? "http://fear.master.com/fear/api/" 
-                 : "http://fear.master.com:4000/fear/api/";
+                 : "http://localhost:4000/fear/api/";
 
 const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
                 ? process.env.JWT_TOKEN 
