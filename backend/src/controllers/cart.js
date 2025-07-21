6ea6c5c0d57f9a0ba356865508833f0824727c5f
FearDread <ghaptonstall@gmail.com>
2025-07-21 03:24:56 -0700

    --update cart controller with custom crud methods and apply to routes

--- backend/src/controllers/cart.js
+++ backend/src/controllers/cart.js
@@ -1,95 +1,53 @@
 const { tryCatch } = require("../libs/handler/error");
 const Cart = require("../models/cart");
 const User = require("../models/user");
-const methods = require("./crud");
 const db = require("../libs/db");
 
-exports.addToCart = tryCatch(async (req, res) => {
-  const { productId, quantity, price } = req.body;
-  const { _id } = req.user;
-
-  db.validate(_id);
-  
-  try {
-    let newCart = await new Cart({
-      userId: _id,
-      productId,
-      price,
-      quantity,
-    }).save();
-    res.json(newCart);
-  } catch (error) {
-    throw new Error(error);
-  }
+exports.create = tryCatch(async (req, res) => {
+  const { _id, productId, quantity, price } = req.body;
+  //db.validate(_id);
+  await new Cart(req.body)
+    .save()
+    .then((created) => { return res.status(200).json({ success: true, result: created }) })
+    .catch(error => new Error(error));
 });
 
-exports.getUserCart = tryCatch(async (req, res) => {
+exports.read = tryCatch(async (req, res) => {
   const { _id } = req.body;
   //db.validate(_id);
   await Cart.find({ userId: _id })
-      .populate("productId")
-      .then((cart) => {
-        console.log('cart data = ', cart);
-        return res.status(200).json({success: true, result: cart})
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
+    .populate("productId")
+    .then((cart) => { return res.status(200).json({ success: true, result: cart }) })
+    .catch(error => new Error(error));
+});
+
+exports.update = tryCatch(async (req, res) => {
+  const { _id } = req.params
+  const { userId, productId, quantity, price } = req.body;
+
+  await Cart.findByIdAndUpdate(_id, req.body, { new: true })
+    .populate('productId')
+    .then((resp) => { return res.status(200).json({ success: true, result: resp }) })
+    .catch(error => new Error(error));
 
 });
 
-exports.removeFromCart = tryCatch(async (req, res) => {
-  const { _id } = req.user;
-  const { cartItemId } = req.params;
-  db.validate(_id);
-  try {
-    const deleteProductFromcart = await Cart.deleteOne({
-      userId: _id,
-      _id: cartItemId,
-    });
-
-    res.json(deleteProductFromcart);
-  } catch (error) {
-    throw new Error(error);
-  }
+exports.delete = tryCatch(async (req, res) => {
+  const { _id } = req.body;
+ // db.validate(_id);
+  await Cart.deleteMany({ userId: _id })
+    .then((resp) => { return res.status(203).json({ success: true }) })
+    .catch(error => new Error(error));
 });
 
-exports.emptyCart = tryCatch(async (req, res) => {
-  const { _id } = req.user;
-  db.validate(_id);
-  try {
-    const deleteCart = await Cart.deleteMany({
-      userId: _id,
-    });
+exports.updateQuantity = tryCatch(async (req, res) => {
+  const { _id } = req.params;
+  const { userId, cartItemId, newQuantity } = req.body;
+  const item = await Cart.findOne({ _id: cartItemId, userId: _id });
 
-    res.json(deleteCart);
-  } catch (error) {
-    throw new Error(error);
-  }
+  item.quantity = newQuantity;
+  item
+    .save()
+    .then((resp) => { return res.status(200).json({ success: true, result: resp }) })
+    .catch(error => new Error(error));
 });
-
-exports.updateProductQuantityFromCart = tryCatch(async (req, res) => {
-  const { _id } = req.user;
-  const { cartItemId, newQuantity } = req.params;
-  db.validate(_id);
-  try {
-    const cartItem = await Cart.findOne({
-      userId: _id,
-      _id: cartItemId,
-    });
-    cartItem.quantity = newQuantity;
-    cartItem.save();
-    res.json(cartItem);
-  } catch (error) {
-    throw new Error(error);
-  }
-});
-
-/*
-const crud = methods.crudController( Cart );
-for(prop in crud) {
-  if(crud.hasOwnProperty(prop)) {
-    module.exports[prop] = crud[prop];
-  }
-}
-*/
--- backend/src/routes/cart.js
+++ backend/src/routes/cart.js
@@ -2,19 +2,11 @@ const router = require("express").Router();
 const Cart = require("../controllers/cart");
 const { isAuthorized, isAdmin } = require("../controllers/auth");
 
-router.post("/", Cart.addToCart);
-router.get("/all", Cart.getUserCart);
-router.get("/user", Cart.getUserCart);
 
-router.delete(
-  "/product/delete/:id",
-  Cart.removeFromCart
-);
-router.delete(
-  "/product/update/:id/:quantity",
-  Cart.updateProductQuantityFromCart
-);
-
-router.delete("/empty", Cart.emptyCart);
+router.post("/", Cart.create)
+router.get("/user/all", Cart.read)
+router.patch("/user/:id", Cart.update)
+router.delete("/user/empty", Cart.delete)
+router.patch("/quantity/:id", Cart.updateQuantity);
 
 module.exports = router;
