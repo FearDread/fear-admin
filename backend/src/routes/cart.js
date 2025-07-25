8ae02fd4db141deee3b662e6bd1e85453e2f4e96
FearDread <ghaptonstall@gmail.com>
2025-07-25 01:43:24 -0700

    --update backend cart logic and minor auth fixes

--- backend/src/routes/cart.js
+++ backend/src/routes/cart.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
+const { tryCatch } = require("../libs/handler/error");
 const Cart = require("../controllers/cart");
 const { isAuthorized, isAdmin } = require("../controllers/auth");
 
 
-router.post("/", Cart.create)
+router.post("/new", tryCatch(Cart.create))
 router.get("/all", Cart.read)
 router.patch("/user/:id", Cart.update)
 router.delete("/user/empty", Cart.delete)
