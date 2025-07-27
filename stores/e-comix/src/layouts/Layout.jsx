0be2c7071299c5bc0d204df055ef4d2804643b7b
FearDread <ghaptonstall@gmail.com>
2025-07-26 21:30:03 -0700

    --apply correct api call for cart slice

--- stores/e-comix/src/layouts/Layout.jsx
+++ stores/e-comix/src/layouts/Layout.jsx
@@ -42,7 +42,7 @@ const Layout = (props) => {
     console.log('layout products = ', products);
     console.log('layout products from prop', props)
     console.log('user stored ', user);
-    if ( user ) store.dispatch(Cart.fetch({id: user._id}));
+    if ( user ) store.dispatch(Cart.getUserCart({id: user._id}));
 
   }, [])
   return (
