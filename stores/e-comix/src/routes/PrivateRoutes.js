e86c1cd88b69c1f981bf8436185e12b52dccf218
FearDread <ghaptonstall@gmail.com>
2025-07-30 15:05:43 -0700

    --build latest feature-factory and apply to e-comix store frontend

--- stores/e-comix/src/routes/PrivateRoutes.js
+++ stores/e-comix/src/routes/PrivateRoutes.js
@@ -5,8 +5,8 @@ import { store } from "../features/store";
 export const PrivateRoutes = ({ children }) => {
   //const userState = useSelector( state => state.user.data )
   const localData = store.local.has("auth") ? store.local.get("auth") : undefined;
-  
-  return (localData.user && localData.token !== undefined) ? (
+  const userState = useSelector( state => state.user.data.user );
+  return (localData && localData.token !== undefined) ? (
     children
   ) : (
     <Navigate to="/login" replace={true} />
