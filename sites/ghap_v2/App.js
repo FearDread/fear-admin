50cd146e13b6869eca442f82a5ba29be6c965e5e
FearDread <ghaptonstall@gmail.com>
2025-12-04 01:00:01 -0500

    --update ghap v2 with latest images and home page video

--- sites/ghap_v2/App.js
+++ sites/ghap_v2/App.js
@@ -2,20 +2,14 @@ const FearServer = require('../../backend/src/FEARServer');
 
 async function main() {
     const server = new FearServer();
-
-    try {
-
-        await server.initialize({
+    
+    server.initialize({
             root: __dirname,
             app: '/public',
             build: 'public'
-        });
-        await server.startServer();
-
-    } catch (error) {
-        console.error('Failed to start application:', error);
-        process.exit(1);
-    }
+        })
+        .then(() => server.startServer())
+        .catch((error) => process.exit(1))
 }
 
 // Handle top-level errors
