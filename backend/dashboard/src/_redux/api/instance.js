6f948537a50e2ab92b71613b8ebef8edc505d7f7
FearDread <ghaptonstall@gmail.com>
2025-12-10 18:35:13 -0500

    --update redux api instance and config to use https when in production

--- backend/dashboard/src/_redux/api/instance.js
+++ backend/dashboard/src/_redux/api/instance.js
@@ -7,8 +7,8 @@ import qs from "qs";
 import storePersist from "../storePersist";
 
 const API_URL = (process.env.NODE_ENV === "production")
-                 ? "http://fear.master.com/fear/api/" 
-                 : "http://localhost:4000/fear/api/";
+                 ? "https://fear.dedyn.io/fear/api/" 
+                 : "http://fear.master.com:4000/fear/api/";
                  
 const API_STATIC_URL = "http://localhost:4000/fear/api/";
 const ACCESS_TOKEN_NAME = (process.env.JWT_TOKEN) 
