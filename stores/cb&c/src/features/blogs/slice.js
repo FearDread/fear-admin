0a7e8ea3b033f8b430a90e11e695fea880367289
FearDread <ghaptonstall@gmail.com>
2025-12-15 16:21:32 +0000

    --remove obsolete store and generate new react-components for efear store

--- stores/cb&c/src/features/blogs/slice.js
+++
@@ -1,5 +0,0 @@
-import { FeatureFactory, ThunkFactory } from "@feardread/feature-factory";
-
-export const { slice: blogSlice, asyncActions: Blog } = FeatureFactory('blog').create();
-
-export default { blogSlice, Blog };
--- stores/cb&c/src/features/brands/slice.js
+++
@@ -1,5 +0,0 @@
-import { FeatureFactory, ThunkFactory } from "@feardread/feature-factory";
-
-export const { slice: brandSlice, asyncActions: Brand } = FeatureFactory('brand').create();
-
-export default { brandSlice, Brand }
