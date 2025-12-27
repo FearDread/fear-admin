b95b23cd965c193e5f95ad121279e02278fbe47f
FearDread <ghaptonstall@gmail.com>
2025-12-26 21:16:50 -0500

    --update backend order model and controller, add new payment routes, model, and controller

---
+++ backend/src/routes/payment.js
@@ -0,0 +1,17 @@
+const Payment = require('../controllers/payment');
+
+module.exports = (fear) => {
+    const router = fear.createRouter();
+    const handler = fear.getHandler();
+    const validator = fear.getValidator();
+
+    router.get("/all", Payment.list);
+    router.post("/new", Payment.create);
+    
+    router.route('/:id')
+        .get(Payment.read)
+        .put(Payment.update);
+
+
+    return router;
+}
