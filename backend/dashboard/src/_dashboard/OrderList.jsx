76e4fe3abc1f3422d77f01a6afebd95503329695
FearDread <ghaptonstall@gmail.com>
2025-12-29 00:17:17 -0500

    --update dashboard orders section with live api calls and updated react table

--- backend/dashboard/src/_dashboard/OrderList.jsx
+++ backend/dashboard/src/_dashboard/OrderList.jsx
@@ -1,179 +1,134 @@
-import React, {useState , useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { useSelector, useDispatch } from "react-redux";
-import { getAllOrders, clearErrors  , deleteOrder} from "_actions/orderAction";
-import { useAlert } from "react-alert";
-import { DELETE_ORDER_RESET } from "_constants/orderConstant";
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardTitle,
+  Row,
+  Col,
+} from "reactstrap";
+import { useHistory } from "react-router-dom";
+import Loader from "components/Loader/Loading";
+import ReactTable from "components/ReactTable/ReactTable.js";
+import ReactTableActions from "components/ReactTable/ReactTableActions.js";
+import * as OrderActions from "_redux/order/actions";
+import * as OrderTypes from "_redux/order/types";
+import ReactBSAlert from "react-bootstrap-sweetalert";
 
 function OrderList() {
   const dispatch = useDispatch();
   const history = useHistory();
-  const alert = useAlert();
- 
-  const { error, loading, orders } = useSelector((state) => state.allOrders);
-  const { error: deleteError, isDeleted } = useSelector(
-    (state) => state.deleteUpdateOrder
-  );
-   const [toggle, setToggle] = useState(false);
+  const [alert, setAlert] = useState(null);
+  const [toggle, setToggle] = useState(false);
 
-  // togle handler =>
-  const toggleHandler = () => {
-    console.log("toggle");
-    setToggle(!toggle);
-  };
-
-  // to close the sidebar when the screen size is greater than 1000px
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth > 999 && toggle) {
-        setToggle(false);
-      
-
-      }
-    };
-       
-          
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, [toggle]);
-
-
-  // dispatching the action
-  useEffect(() => {
-    if (error) {
-      alert.error(error);
-      dispatch(clearErrors());
-    }
-    if(deleteError){
-      alert.error(deleteError)
-      dispatch(clearErrors())
-    }
-    if(isDeleted){
-          alert.success("Order Deleted Successfully");
-             history.push("/admin/orders");
-             dispatch({ type: DELETE_ORDER_RESET });
-
-    }
-    dispatch(getAllOrders());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dispatch, error, alert , isDeleted , deleteError]);
-
+  const { error, loading, orders } = useSelector((state) => state.order);
+  const header = [
+    { Header: "Order ID", accessor: "_id" },
+    { Header: "Status", accessor: "status" },
+    { Header: "Items", accessor: "items" },
+    { Header: "Price", accessor: "price" },
+    { Header: "Actions", accessor: "actions", sortable: true, filterable: false }
+  ];
   // delet order handler
   const deleteOrderHandler = (id) => {
-    dispatch(deleteOrder(id))
+    dispatch(OrderActions.remove(id));
+    dispatch({ type: OrderTypes.DELETE_ORDER_RESET });
+    hideAlert()
+  };
+  const confirmDelete = (_id) => {
+    setAlert(
+      <ReactBSAlert
+        warning
+        style={{ display: "block", marginTop: "-100px" }}
+        title="Are you sure?"
+        onConfirm={() => deleteOrderHandler(_id)}
+        onCancel={() => hideAlert()}
+        confirmBtnBsStyle="success"
+        cancelBtnBsStyle="danger"
+        confirmBtnText="Yes, delete it!"
+        cancelBtnText="Cancel"
+        showCancel
+        btnSize="">
+        Your sure you want to delete this product?
+      </ReactBSAlert>
+    );
   };
 
-  // DATA GRID VALUE
-const columns = [
-  {
-    field: "id",
-    headerName: "Order ID",
-    minWidth: 120,
-    flex: 0.7,
-    headerClassName: "column-header",
-  },
-  {
-    field: "status",
-    headerName: "Status",
-    minWidth: 100,
-    flex: 0.8,
-    headerClassName: "column-header hide-on-mobile",
-    renderCell: (params) => {
-      const color = params.value === "Delivered" ? "green" : "red";
-      return <div style={{ color, fontWeight: 600 }}>{params.value}</div>;
-    },
-  },
-  {
-    field: "itemsQty",
-    headerName: "Items Qty",
-    type: "number",
-    minWidth: 120,
-    flex: 0.8,
-    headerClassName: "column-header hide-on-mobile",
-  },
-  {
-    field: "amount",
-    headerName: "Amount",
-    type: "number",
-    minWidth: 120,
-    flex: 0.8,
-    headerClassName: "column-header hide-on-mobile",
-  },
-  {
-    field: "actions",
-    headerName: "Actions",
-    flex: 1.5,
-    sortable: false,
-    minWidth: 150,
-    headerClassName: "column-header1",
-    renderCell: (params) => {
-      return (
+  const hideAlert = () => {
+    setAlert(null);
+  };
+
+  const displayOrders = () => {
+    let dataTable = [];
+
+    if (orders && orders.length > 0) {
+      orders.map((item, key) => {
+        dataTable.push({
+          id: item._id,
+          status: item.status,
+          price: "$" + item.orderPrice,
+          items: item.items || 1,
+          actions: (
+            ReactTableActions(key, (() => {
+              console.log("edit order ::", item);
+              history.push("/admin/order/edit/" + item._id);
+
+            }),
+              (() => {
+                console.log("remove order :: ", item);
+                confirmDelete(item._id, "order", deleteOrderHandler, hideAlert);
+                //confirmDelete(item._id);
+              })
+            ))
+        });
+      })
+    }
+
+    return dataTable;
+  }
+
+  useEffect(() => {
+
+    dispatch(OrderActions.list());
+
+  }, [dispatch]);
+
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
         <>
-          <Link to={`/admin/order/${params.getValue(params.id, "id")}`}>
-            <EditIcon className="icon-" />
-          </Link>
-          <Link
-            onClick={() => deleteOrderHandler(params.getValue(params.id, "id"))}
-          >
-            <DeleteIcon className="iconbtn" />
-          </Link>
+          <div className="content">
+            {alert}
+            <Row>
+              <Col className="mb-5" md="12">
+                <Card>
+                  <CardHeader>
+                    <CardTitle tag="h4">ALL ORDERS</CardTitle>
+                  </CardHeader>
+                  <CardBody>
+                    <ReactTable
+                      data={displayOrders()}
+                      filterable
+                      resizable={false}
+                      columns={header}
+                      defaultPageSize={10}
+                      showPaginationTop
+                      showPaginationBottom={true}
+                      className="-striped -highlight"
+                    />
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </div>
         </>
-      );
-    },
-  },
-];
+      )}
 
-
- 
-
-  const rows =[];
-  orders && orders.forEach(item =>{
-
-    rows.push({
-      id: item._id,
-      itemsQty: item.orderItems.length,
-      amount: item.totalPrice,
-      status: item.orderStatus,
-    });
-  })
-
-   return (
-     <>
-       {loading ? (
-         <Loader />
-       ) : (
-         <>
-           <MetaData title={`ALL Orders - Admin`} />
-
-           <div className="product-list" style={{ marginTop: 0 }}>
-             <div className={!toggle ? "listSidebar" : "toggleBox"}>
-               <Sidebar />
-             </div>
-
-             <div className="list-table">
-               <Navbar toggleHandler={toggleHandler} />
-               <div className="productListContainer">
-                 <h4 id="productListHeading">ALL ORDERS</h4>
-
-                 <DataGrid
-                   rows={rows}
-                   columns={columns}
-                   pageSize={10}
-                   disableSelectionOnClick
-                   className="productListTable"
-                   autoHeight
-                 />
-               </div>
-             </div>
-           </div>
-         </>
-       )}
-     </>
-   );
+    </>
+  );
 }
 
 export default OrderList;
