059a7af601c3c6b3c61c422dafc2d7114b5cd16e
FearDread <ghaptonstall@gmail.com>
2026-01-03 02:28:26 -0500

    --add logout handler to account sidebar, update handler lib and auth controller

--- backend/src/libs/handler/index.js
+++ backend/src/libs/handler/index.js
@@ -20,6 +20,7 @@ class AppError extends Error {
  * @returns {Function} Express middleware function
  */
 const asyncHandler = (fn) => {
+  console.log('fn = ', fn);
   return (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
   };
