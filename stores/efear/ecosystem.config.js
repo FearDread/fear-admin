31c66c50fdfa13f6840fb5afc3e8d9937be48fa5
FearDread <ghaptonstall@gmail.com>
2026-02-11 14:49:24 +0000

    --update ecosystem configs for main server and efear

--- stores/efear/ecosystem.config.js
+++ stores/efear/ecosystem.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   apps : [{
     name   : "efear",
-    script : "yarn run start"
+    script : "yarn run start",
+    instances: '2',
+    exec_mode: 'cluster',
+    env: {
+      NODE_ENV: 'production'
+    },
+    env_development: {
+      NODE_ENV: 'development'
+    }
   }]
 }
